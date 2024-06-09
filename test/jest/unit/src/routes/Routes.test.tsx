46845c9e87ab831662e1332ff8af43a6f9a707ae
Daniel Ferrari <daniel.ferrari@filewave.com>
2024-06-09 17:50:23 +0200

    fix: fixing codcov

--- test/jest/unit/src/routes/Routes.test.tsx
+++ test/jest/unit/src/routes/Routes.test.tsx
@@ -4,5 +4,5 @@ import { Routes } from 'src/routes';
 
 test('renders learn react link', () => {
   render(<Routes />);
-  expect(screen.queryByText(/Hello world/i)).not.toBeInTheDocument();
+  expect(screen.getByText(/Hello world/i)).toBeInTheDocument();
 });
